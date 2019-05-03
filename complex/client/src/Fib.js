977bf4113bbda539700bdfe10fc8fb48c22c1dab
Alex Britton <alex@ahbritton.com>
2019-05-03 06:45:33 +0100

    corrections

--- complex/client/src/Fib.js
+++ complex/client/src/Fib.js
@@ -13,28 +13,26 @@ class Fib extends Component {
         this.fetchIndexes();
     }
 
-    async fetchValues(){
-        const values = await axios.get('/api/values/current');
-        this.setState({
-            values: values.data
-        }); 
-    }
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
+  }
 
-    fetchIndexes(){
-        const seenIndexes = await axios.get('/api/values/all');
-        this.setState({
-            seenIndexes: seenIndexes.data
-        });
-    }
-
-    handleSubmit = async (event => {
-        event.preventDefault();
-
-        await axios.post('/api/values', {
-            index: this.state.index
-        });
-        this.setState({index: ''});
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
     });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+    this.setState({ index: '' });
+  };
 
     renderSeenIndexes(){
         return this.state.seenIndexes.map( ({ number }) => number).join(', ');
@@ -48,30 +46,32 @@ class Fib extends Component {
                 <div key={key}> 
                     For index {key} I calculated {this.state.values[key]}
                 </div>
-            )
-        }
+      );
     }
-  
-    render(){
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Enter your index:</label>
-                    <input 
-                        value={this.state.index}
-                        onChange={event => this.setState({ index: event.target.value })}
-                    />
-                    <button>Submit</button>
-                </form>
-     
-                <h3>Indexes I have seen:</h3>
-                {this.renderSeenIndexes()}
-     
-                <h3>Calculated values</h3>
-                {this.renderValues()}
-            </div>
-        );
-    }
+
+    return entries;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
+
+        <h3>Calculated Values:</h3>
+        {this.renderValues()}
+      </div>
+    );
+  }
 }
 
 export default Fib;
--- complex/server/index.js
+++ complex/server/index.js
@@ -11,14 +11,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Postgres Client Setup
-
-const { pool } = reqiure('pg');
-const pgClient = new pool;({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
 });
 
 pgClient.on('error', () => console.log('Lost PG connection'));
@@ -39,21 +38,22 @@ const redisClient = redis.createClient({
 
 const redisPublisher = redisClient.duplicate();
 
-// Expres Route Handlers
+// Express route handlers
 
 app.get('/', (req,res) => {
     res.send('Hi');
 });
 
-app.get('/values/all', async(req,res) => {
-    const values = await pgClient.query('SELECT * from values')
-    res.send(values.rows);
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
 });
 
-app.get('/values/current', async(req,res) => {
-    redisClient.hgetall('values', (err, values) => {
-        res.send('values'); 
-    });
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
 });
 
 app.post('/values', async(req,res) => {
